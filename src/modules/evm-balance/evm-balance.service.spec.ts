a74cd0b70017a8e57880261833950dbd77d431f9
Simon Diaz <simon.diaz.abad@gmail.com>
2023-08-25 17:37:22 -0400

    added getERC20balance test

--- src/modules/evm-balance/evm-balance.service.spec.ts
+++ src/modules/evm-balance/evm-balance.service.spec.ts
@@ -2,9 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EvmBalanceService } from './evm-balance.service';
 import { AppConfigService } from '@config/app-config.service';
 import { createMock } from '@golevelup/ts-jest';
-import { Blockchains } from '@constants';
+import { Blockchains, ERC20Tokens } from '@constants';
 import { HttpException } from '@nestjs/common';
-import { mockProviderGetBalance } from '@tests/mocks';
+import { mockProviderGetBalance, mockGetERC20Balance } from '@tests/mocks';
 
 describe('EvmBalanceService', () => {
     let evmBalanceService: EvmBalanceService;
@@ -13,6 +13,7 @@ describe('EvmBalanceService', () => {
 
     beforeEach(async () => {
         mockProviderGetBalance(mockBalance);
+        mockGetERC20Balance(mockBalance);
 
         const module: TestingModule = await Test.createTestingModule({
             providers: [EvmBalanceService],
@@ -50,6 +51,19 @@ describe('EvmBalanceService', () => {
         ).toThrowError(HttpException);
     });
 
-    it.todo('should get ERC20 balance', async () => {});
-    it.todo('should throw exception on undefined provider of getERC20Balance', async () => {});
+    it('should get ERC20 balance', async () => {
+        appConfigService.providers[Blockchains.ETH] = 'https://eth.llamarpc.com';
+        appConfigService.erc20TokenAddress[Blockchains.ETH] = {};
+        appConfigService.erc20TokenAddress[Blockchains.ETH][ERC20Tokens.USDT] =
+            '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+
+        const balance = await evmBalanceService.getERC20Balance(
+            Blockchains.ETH,
+            ERC20Tokens.USDT,
+            '0x0089d53f703f7e0843953d48133f74ce247184c2',
+        );
+
+        expect(balance.toString()).toEqual(mockBalance.toString());
+    });
+    it.todo('should throw exception on undefined provider of getERC20Balance');
 });
